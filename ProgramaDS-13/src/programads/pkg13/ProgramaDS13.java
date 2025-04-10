87200911937f97ee0cc92b015eae5b6a6c5e08d4
ArthurOliCode <arthur.rilber@gmai.com>
2025-04-10 14:16:42 -0300

    Atualização arquivo 13DS

--- ProgramaDS-13/src/programads/pkg13/ProgramaDS13.java
+++ ProgramaDS-13/src/programads/pkg13/ProgramaDS13.java
@@ -21,6 +21,8 @@ public class ProgramaDS13 {
          massa = Massa do usuário
          gen = Genêro do usuário
          altura = Altura do usuário
+         imc = Indíce de massa corporal do usuário
+         pesoI = Peso ideal de acordo com o genêro
         */
         
         System.out.println("     Bem Vindo!");
@@ -37,9 +39,46 @@ public class ProgramaDS13 {
         
         String nome = saidaString(teclado, "Por favor, digite seu nome: ");
         
-        float massa = testarFloat(teclado, "Indique sua massa corporal atual: ");
+        double massa = testarFloat(teclado, "Indique sua massa corporal atual: ");
+        double altura = testarFloat(teclado, "Escreva sua altura em metros: ");
         String gen = testGen(teclado, "Insira o seu genero biologico: ");
         
+        double imc = massa / (altura * altura);
+        double pesoI = 0;
+        
+        
+        
+        if(imc > 0){
+            pesoI = (72.7 * altura) - 58;
+            if(gen.equals("m") && imc == pesoI){
+                resultados(nome, massa, altura, "Ideal");
+            }
+            else{
+                pesoI = (62.1 * altura) - 44.7;
+                if(gen.equals("f") && imc == pesoI){
+                    resultados(nome, massa, altura, "Ideal");
+                }
+                else if(imc < 18.5){
+                    resultados(nome, massa, altura, "Magreza");
+
+                }
+                else if(imc <= 24.9){
+                    resultados(nome, massa, altura, "Normal");
+                }
+                else if(imc <= 29.9 ){
+                    resultados(nome, massa, altura, "Sobrepeso");
+                }
+                else if(imc <= 34.9){
+                    resultados(nome, massa, altura, "Obesidade I");
+                }
+                else if(imc <= 39.9){
+                    resultados(nome, massa, altura, "Obesidade II");
+                }
+                else if(imc >= 40){
+                    resultados(nome, massa, altura, "Obesidade III");
+                }  
+            }
+        }  
     }
     
     
@@ -89,5 +128,15 @@ public class ProgramaDS13 {
             }
         }
         
+    }
+    
+    public static void resultados(String nome, double massa, double altura, String status){
+        System.out.printf("%-10s Tabela de Resultados\n", " ");
+        System.out.printf("----------------------------------------\n");
+        System.out.printf("%-5s: %-5s \n", "Nome", nome);
+        System.out.printf("%-5s: %-3s %.2f\n", "Peso", "", massa);
+        System.out.printf("%-5s: %-2s %.2f\n", "Altura", "", altura);
+        System.out.printf("%-5s: %-1s %s\n", "Situacao", "", status);
+        System.out.println("-----------------------------------\n\n");
     }
 }
