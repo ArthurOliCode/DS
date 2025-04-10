2b20d5d5a710dc5d0890945e5a2542deac2f7bb4
Arthur Oliveira <arthur.rilber@gmail.com>
2025-04-10 14:30:14 -0300

    Update ProgramaDS14.java
    Atualização do código

--- ProgramaDS-14/src/programads/pkg14/ProgramaDS14.java
+++ ProgramaDS-14/src/programads/pkg14/ProgramaDS14.java
@@ -3,18 +3,142 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package programads.pkg14;
+import java.util.Scanner;
 
 /**
  *
- * @author CAMARGO
+ * @author ArthurOliCode
  */
 public class ProgramaDS14 {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+         teclado = entrada de dados
+         quantL = quantidade de litros a serem vendidos
+         descL = Desconto por quantidade de litro vendida
+         tipoCom = Tipo de combustível que o cliente prefere
+         taxaCom = Taxa do comustível em específico
+         precL = Preço total sobre a quantidade de litros do combustível escolhido
+         alcV = Valor do combustível álcool
+         gasV = Valor do combustível gasolina
+        */
+        
+        
+        System.out.println("     Bem Vindo!");
+        System.out.println("""
+                           Este ira calcular o preco unitario do tipo de 
+                           combustivel escolhido pelo cliente, alem disso, 
+                           havera um desconto pela quantidade de litro,
+                           que ira depender da escolha do usuario. 
+                           Ao final, o programa apresentara os dados.
+                           """);
+        System.out.println("\n");
+        
+        
+        System.out.printf("%-10s Tabela de Precos\n", " ");
+        System.out.printf("-----------------------------------\n");
+        System.out.printf("%-5s: %-10s | %-10s \n", "Combustivel", "Gasolina", "Alcool");
+        System.out.printf("%-5s: %-5s %-10s | %-10s \n", "Preco", "","R$3.30", "R$2.90");
+        System.out.println("-----------------------------------\n\n");
+        
+        
+        Scanner teclado = new Scanner(System.in);
+        double descL = 4 * 0.01; 
+        String taxaCom = "";
+        
+        float quantL = testFloat(teclado, "Digite a quantidade de Litros necessaria para o tanque do seu automovel: ");
+        
+        String tipoCom = testResp(teclado, "Informe sua preferencia de combustivel: ");
+        
+        
+        if (tipoCom.equals("g")){
+            taxaCom = "3.30";
+            
+            if (quantL > 20){
+                descL = 6 * 0.01;
+            }
+            
+            
+        }else if(tipoCom.equals("a")){
+            
+        }
+        
+        System.out.println(descL);
+        double precL = (quantL * Float.parseFloat(taxaCom)) - (quantL * descL); 
+        
+        System.out.println(descL);
+        
+        System.out.printf("%-10s Tabela de Resultados\n", " ");
+        System.out.printf("----------------------------------------\n");
+        System.out.printf("%-5s: %-10s \n", "Combustivel", tipoCom);
+        System.out.printf("%-5s: %-5s %.2f\n", "Preco", "", precL);
+        System.out.println("-----------------------------------\n\n");
+        
+        
+        
     }
     
+    
+    public static int testInt(Scanner teclado, String msg){
+        int num;
+        
+        while(true){
+            
+            try{
+                
+                System.out.println(msg);
+                String entrada = teclado.next();
+                
+                num = Integer.parseInt(entrada);
+                return num;
+                
+            }catch(NumberFormatException e){
+                System.out.println("\n\nEntrada invalida, por favor, insira um numero inteiro valido");
+                System.out.println("--------------------------------------------------------\n\n");
+            }
+        }
+    }
+    
+    
+    public static float testFloat(Scanner teclado, String msg){
+        float num;
+        
+        while(true){
+            
+            try{
+                
+                System.out.println(msg);
+                String entrada = teclado.next();
+                
+                num = Float.parseFloat(entrada);
+                return num;
+                
+            }catch(NumberFormatException e){
+                System.out.println("\n\nEntrada invalida, por favor, insira um numero valido");
+                System.out.println("--------------------------------------------------------\n\n");
+            }
+        }
+    }
+    
+    public static String testResp(Scanner teclado, String msg){
+        String resp = "";
+        String entrada = ""; 
+
+        while(!entrada.equals("g") && !entrada.equals("a"))
+        {
+            System.out.println(msg);
+            entrada = teclado.next();
+            if(entrada.substring(0, 1).toLowerCase().equals("g") || entrada.substring(0, 1).toLowerCase().equals("a")){
+                resp = entrada; 
+            }else{
+                System.out.println("\n\nEntrada invalida, por favor, escolha entre alcool ou gasolina!");
+                System.out.println("------------------------------------------------------------------\n\n");
+            }
+        }
+        return resp;       
+    } 
 }
