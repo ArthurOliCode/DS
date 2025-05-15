4ec318c36f9455c8546f78a2c04cf4857688dead
ArthurOliCode <arthur.rilber@gmail.com>
2025-05-15 15:14:23 -0300

    Adicionando o primeiro exercício de introdução a POO

---
+++ POO/ex001POO/src/ex001poo/Automovel.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex001poo;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public abstract class Automovel {
+    //Definindo encapsulamento e herança com uma classe autmóvel
+    //Exemplo de Programação orientada a objeto POO
+    
+    
+    protected int velocidade;
+    protected int ano;
+    protected String marca;
+    protected float combus;
+    protected int rodas;
+    protected float peso;
+    protected float valor;
+    protected boolean farol;
+    protected boolean piscaA;
+    protected boolean direcao;
+    protected boolean seta; 
+    
+    public abstract int acelerar();
+    public abstract int diminuir();
+    public abstract void farolLigado();
+    public abstract void farolDesligado();
+    public abstract void piscaAlertaLigado();
+    public abstract void piscaAlertaDesligado();
+    public abstract void virarDirecao();
+    public abstract void setaLigada();
+    public abstract void buzinar();
+
+    
+    
+}
---
+++ POO/ex001POO/src/ex001poo/Onibus.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex001poo;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Onibus extends Automovel {
+
+    @Override
+    public int acelerar() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public int diminuir() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void farolLigado() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void farolDesligado() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void piscaAlertaLigado() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void piscaAlertaDesligado() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void virarDirecao() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void setaLigada() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void buzinar() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+}
