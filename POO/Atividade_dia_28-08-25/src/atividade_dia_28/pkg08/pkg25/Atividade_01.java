491cd10eada6443b597be6c6e78d84db4787a51a
ArthurOliCode <arthur.rilber@gmail.com>
2025-09-15 23:59:18 -0300

    Adicionando o exercício do dia 28 de Agosto sobre POO

---
+++ POO/Atividade_dia_28-08-25/src/atividade_dia_28/pkg08/pkg25/Atividade_01.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade_dia_28.pkg08.pkg25;
+
+/**
+ *
+ * @author arthu_uhkw4r8
+ */
+
+public class Atividade_01 {
+    
+    public void HelloWord(){
+        int c = 0; // Variável temporária com função de contador.
+        
+        while(c < 20){
+            System.out.printf("-");
+            c++;
+        }
+        
+        String frase = "Hello Word";
+        System.out.println("");
+        System.out.printf("%10s", frase);
+        c = 0;
+        
+        while(c < 20){
+            System.out.printf("-");
+            c++;
+        }
+    }
+}
---
+++ POO/Atividade_dia_28-08-25/src/atividade_dia_28/pkg08/pkg25/Atividade_dia_280825.java
@@ -0,0 +1,18 @@
+
+package atividade_dia_28.pkg08.pkg25;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ArthurOliCode
+ */
+
+
+public class Atividade_dia_280825 {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
