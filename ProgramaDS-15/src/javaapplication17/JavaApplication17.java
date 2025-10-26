ad6b4be60f45824d22dd73b440baa60d4ecfcd69
ArthurOliCode <arthur.rilber@gmail.com>
2025-10-26 18:01:14 -0300

    Adicionando o exercício Antecessor e sucessores

--- ProgramaDS-15/src/javaapplication17/JavaApplication17.java
+++ ProgramaDS-15/src/javaapplication17/JavaApplication17.java
@@ -18,19 +18,22 @@ public class JavaApplication17 {
 //        Medias medias[] = new Medias[2];
 //        AumentoSalario salario[] = new AumentoSalario[2];
 //        IpiProdutos produto[] = new IpiProdutos[2];
-        SalarioMinimo salario[] = new SalarioMinimo[2];
+//        SalarioMinimo salario[] = new SalarioMinimo[2];
+        SucessorAntecessor num[] = new SucessorAntecessor[2];
         
 //        idades[0] = new IdadeExtenso();
 //        medias[0] = new Medias();
 //        salario[0] = new AumentoSalario();
 //        produto[0] = new IpiProdutos();
-        salario[0] = new SalarioMinimo();
+//        salario[0] = new SalarioMinimo();
+        num[0] = new SucessorAntecessor();
         
 //        System.out.printf("\n\n Media final: %s \n\n", medias[0].toString());
 //        System.out.printf("\n\n Idade por extenso: %s \n\n", idades[0].toString());
 //        System.out.printf("\n\n resultados: %s\n\n",  salario[0].toString());
 //        System.out.printf("\n\nResultados: %s\n\n", produto[0].toString());
-        System.out.printf("\n\nResultados: %s\n\n", salario[0].toString());
+//        System.out.printf("\n\nResultados: %s\n\n", salario[0].toString());
+        System.out.printf("\n\nResultados: %s\n\n", num[0].toString());
     }
 
 }
--- ProgramaDS-15/src/javaapplication17/SucessorAntecessor.java
+++ ProgramaDS-15/src/javaapplication17/SucessorAntecessor.java
@@ -4,10 +4,58 @@
  */
 package javaapplication17;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ArthurOliCode
  */
 public class SucessorAntecessor {
+    int num;
+    int ant; // antecessor ao número fornecido
+    int suces; // sucessor do número fornecido
+    Scanner teclado = new Scanner(System.in);
+
+    public SucessorAntecessor() {
+        TesteEntrada.titulo("Calculadora de Antecessores e Sucessores");
+        setNum();
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum() {
+        int valor = TesteEntrada.testInt(teclado, "Insira o numero inteiro incial: [Ex: 1, 2 ...]");
+        this.num = valor;
+        
+        setAnt(getNum());
+        setSuces(getNum());
+    }
+
+    public int getAnt() {
+        return ant;
+    }
+
+    public void setAnt(int num) {
+        int antecessor = num - 1;
+        this.ant = antecessor;
+    }
+
+    public int getSuces() {
+        return suces;
+    }
+
+    public void setSuces(int num) {
+        int sucessor = num + 1;
+        this.suces = sucessor;
+    }
+
+    @Override
+    public String toString() {
+        return "SucessorAntecessor{" + "numero: " + num + ", seu antecessor: " + ant + ", seu sucessor: " + suces + '}';
+    }
+    
+    
     
 }
